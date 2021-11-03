5c00f5061ec39977498341d290b28986b430b8c8
ReactWEB <reactweb@ukr.net>
2021-11-03 21:50:56 +0200

    umbrella is working

--- js/index.js
+++ js/index.js
@@ -3,20 +3,19 @@
 //Написать функцию чтения этого файла.
 
 const os = require('os');
-const infoOs = {
-  platform: os.platform(),
-  architect: os.arch(),
-  memory: os.freemem(),
-  hostinfo: os.hostname(),
-};
-console.log(infoOs);
-
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'umbrella'));
+/*fs.mkdir(path.join(__dirname, 'umbrella'), err => {
+  if (err) throw new Error(err);
+  console.log('Directory was created');
+});*/
+/*fs.writeFile(path.join(__dirname, 'umbrella', 'info_os.txt'), 'some txt hello', err => {
+  if (err) throw new Error(err);
+  console.log('Fail was created');
+});*/
 
-fs.writeFile(path.join(__dirname, 'umbrella', 'info_os.js'));
-
-const info = fs.readFileSync(path.join(__dirname, 'umbrella', 'info_os.js'), 'utf-8');  
-console.log(info);
+fs.readFile(path.join(__dirname, 'umbrella', 'info_os.txt'), 'utf-8', (err, data) => {
+  if (err) throw new Error(err);
+  console.log(data);
+});  
